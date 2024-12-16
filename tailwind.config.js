ad89a12d0133c0826a1ff7850141c1af1a921efd
Bhanu Prakash Pandey <bhanupandey0843@gmail.com>
2024-12-16 22:02:19 +0530

    Update tailwind.config.js
    remove

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#155436', // Custom primary color
-      },
-    },
-  },
-  plugins: [],
-};
+
